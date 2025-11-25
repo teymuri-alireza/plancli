1d0c94af71554118a560249a4786ce6322364daf
teymuri-alireza <kishijung@gmail.com>
2025-11-25 20:33:08 +0330

    feat: add new-task function menu

--- TaskCLI/Program.cs
+++ TaskCLI/Program.cs
@@ -3,13 +3,15 @@
 
 using Terminal.Gui;
 using Terminal.Gui.Graphs;
+using TaskCLI.Models;
+using System.Text.Json;
 
 class Program
 { 
     static void Main(string[] args)
     {
-        // get date
         var day = DateTime.Now;
+        var db = new DatabaseController("tasks.json");
         // main function
         Application.Init();
 
@@ -27,8 +29,8 @@ class Program
         {
             Normal = Application.Driver.MakeAttribute(Color.White, Color.Black),
         };
-        var menu = new MenuBar(new MenuBarItem[]
-        {
+        var menu = new MenuBar(
+        [
             new("_File", new MenuItem[]
             {
                 new("_Exit", "", () =>
@@ -40,10 +42,10 @@ class Program
             {
                 new("_New", "", () =>
                 {
-                    ;
+                    NewTask(db);
                 }) 
             }),
-        });
+        ]);
         var dayOfTheWeek = new Label(day.ToString("dddd") + " - " + day.ToString("M"))
         {
             X = Pos.Center(),
@@ -56,17 +58,16 @@ class Program
             Width = Dim.Fill(),
             Orientation = Orientation.Horizontal
         };
-        var button = new Button("OK")
+        string tasksText = string.Join("\n", db.Items.Select(t => $"{t.Title} - {t.Description}"));
+        var tasksWindow = new Label(tasksText)
         {
             X = 1,
-            Y = 3
-        };
-        button.Clicked += () =>
-        {
-            MessageBox.Query("Info", "Button clicked", "Ok");
+            Y = 4,
+            Width = Dim.Fill(),
+            Height = Dim.Fill()
         };
 
-        win.Add(dayOfTheWeek, breakLine, button);
+        win.Add(dayOfTheWeek, breakLine, tasksWindow);
         top.Add(menu);
         top.Add(win);
 
@@ -74,5 +75,58 @@ class Program
         Application.Shutdown();
 
         Console.ResetColor();
+    }
+
+    public static void NewTask(DatabaseController db)
+    {
+        var dialog = new Dialog("Add Task", 60, 10);
+        var titleLabel = new Label("Title:")
+        {
+            X = 1,
+            Y = 1,
+        };
+        var titleInput = new TextField("")
+        {
+            X = Pos.Right(titleLabel) + 7,
+            Y = Pos.Top(titleLabel),
+            Width = 40
+        };
+        var descriptionLabel = new Label("Description:")
+        {
+            X = 1,
+            Y = 3,
+        };
+        var descriptionInput = new TextField("")
+        {
+            X = Pos.Right(descriptionLabel) + 1,
+            Y = Pos.Top(descriptionLabel),
+            Width = 40
+        };
+
+        dialog.Add(titleLabel, titleInput);
+        dialog.Add(descriptionLabel, descriptionInput);
+
+        var ok = new Button("OK");
+        ok.Clicked += () =>
+        {
+            var newTask = new TodoItem
+            {
+                Title = titleInput.Text.ToString(),
+                Description = descriptionInput.Text.ToString()
+            };
+            // logic to save into database
+            db.Items.Add(newTask);
+            db.Save();
+
+            Application.RequestStop(); // close dialog
+        };
+
+        var cancel = new Button("Cancel");
+        cancel.Clicked += () => Application.RequestStop();
+
+        dialog.AddButton(ok);
+        dialog.AddButton(cancel);
+
+        Application.Run(dialog);
     }
 }
