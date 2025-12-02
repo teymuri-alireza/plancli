81f23a4271a74dee6b6501fefa1e53da9f1b3a67
teymuri-alireza <kishijung@gmail.com>
2025-12-02 15:41:41 +0330

    fix: add task completing function for arguments

--- PlanCLI/Arguments.cs
+++ PlanCLI/Arguments.cs
@@ -22,7 +22,10 @@ class Arguments
             case "--reset":
                 ResetTask(db);
                 break;
-            
+            case "-c":
+            case "--check":
+                CheckTheTask(args, db);
+                break;
             default:
                 Console.WriteLine("Unkown Option.");
                 break;
@@ -31,10 +34,16 @@ class Arguments
 
     static void ListTasks(DatabaseController db)
     {
+        if (db.Items.Count == 0)
+        {
+            Console.WriteLine("Task list is empty");
+            return;
+        }
+
         foreach (var task in db.Items)
         {
             var mark = task.IsDone ? "[x]" : "[ ]";
-            Console.WriteLine($"{mark}  {task.Title} - {task.Description}");
+            Console.WriteLine($"{mark} {task.Id}. {task.Title}");
         }
     }
 
@@ -57,11 +66,38 @@ class Arguments
         Console.WriteLine("Task added");
     }
 
+    static void CheckTheTask(string[] args, DatabaseController db)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Error: missing task Id.");
+            return;
+        }
+        try
+        {
+            var taskToDelete = db.Items.FirstOrDefault(t => t.Id == int.Parse(args[1]));
+            if (!string.IsNullOrEmpty(taskToDelete?.Id.ToString()))
+            {
+                taskToDelete.IsDone = true;
+                db.Save();
+            }
+            else
+            {
+                Console.WriteLine("Error: task Id does not exist.");
+                return;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Error: task Id is not an integer!");
+            return;
+        }
+    }
+
     static void ResetTask(DatabaseController db)
     {
         string fileName = "tasks.json";
         File.WriteAllText(fileName, "[]");
-        // Refresh UI
         db.Load();
         db.Save();
         Console.WriteLine("Tasks reset successfully");
@@ -73,5 +109,7 @@ class Arguments
         Console.WriteLine("  dotnet run             \tRun interactive mode (TUI)");
         Console.WriteLine("  dotnet run -- -l       \tList tasks");
         Console.WriteLine("  dotnet run -- -n \"title\"\tAdd new task");
+        Console.WriteLine("  dotnet run -- -c Id      \tcheck a task as complete");
+        Console.WriteLine("  dotnet run -- -r       \tReset tasks list");
     }
 }
